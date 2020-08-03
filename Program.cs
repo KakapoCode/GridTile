41919b0260a95a0d0437698be97e0fbd2fbb6b49
KakapoCode <53716660+KakapoCode@users.noreply.github.com>
2020-08-03 02:02:03 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Testing_Grid
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+                const int maxY = 100;
+                const int maxX = 100;
+                const int min = 5;
+                int colSize = -1;
+                int rowSize = -1;
+
+                //Ask user for grid size. Only accept valid sizes.
+                do {
+                    Console.WriteLine("Enter a height for the grid (5 - 100).");
+                    colSize = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter a width for the grid (5 - 100).");
+                    rowSize = int.Parse(Console.ReadLine());
+                    } while (colSize < min || rowSize < min || colSize > maxX || rowSize > maxY);
+
+                Tile.SetGridSize(colSize, rowSize);
+                Tile[,] tiles = new Tile[colSize, rowSize];
+
+                //Initialize the objects in the grid array. Draw the grid using their symbol. 
+                for (int x = 0; x < colSize; x++)
+                {
+                    for (int y = 0; y < rowSize; y++)
+                    {
+                        tiles[x, y] = new Tile(x, y);
+                        tiles[x, y].ReadSymbol();
+                    }
+                    Console.WriteLine();
+                }
+        }
+
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing_Grid
+{
+    enum Location
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+        Top,
+        Bottom,
+        Left,
+        Right,
+        Mid,
+        Error
+    }
+
+    class Tile
+    {
+        private Location Loc;
+        private string Symbol;
+        private static int Height = 5;
+        private static int Width = 5;
+
+        public Tile(int x, int y)
+        {
+            Loc = Location.Error; //Default to Location.Error, just in case SetLoc somehow fails.
+            SetLoc(x, y);
+            SetSymbol();
+        }
+
+        public void ReadSymbol()
+        {
+            Console.Write(Symbol);
+        }
+
+        //Set our grid size information. Because arrays start at 0, we minus 1. 
+        public static void SetGridSize(int height, int width)
+        {
+            Height = height - 1;
+            Width = width - 1;
+        }
+
+        //Set the symbol of each tile based on its location. 
+        private void SetSymbol()
+        {
+            if (Loc == Location.TopLeft || Loc == Location.TopRight || Loc == Location.Top)
+                Symbol = "^";
+            if (Loc == Location.Left || Loc == Location.Right)
+                Symbol = "|";
+            if (Loc == Location.BottomLeft || Loc == Location.BottomRight || Loc == Location.Bottom)
+                Symbol = "v";
+            if (Loc == Location.Mid)
+                Symbol = "*";
+            if (Loc == Location.Error)
+                Console.WriteLine("Error: A Tile was not assigned a proper location.");
+        }
+
+        //Set the location of the tile, given the index of the 3D array. E.G. tile[0,0] refers to TopLeft of the grid.
+        private void SetLoc(int x, int y)
+        {
+            //Top Left, Top Middle, and Top Right
+            if (x == 0 && y == 0)
+            {
+                Loc = Location.TopLeft;
+            }
+            if (x == 0 && y > 0 && y < Width)
+            {
+                Loc = Location.Top;
+            }
+            if (x == 0 && y == Width)
+            {
+                Loc = Location.TopRight;
+            }
+
+            //Middle Left, Middle, and Middle Right
+            if (x > 0 && x < Height && y == 0)
+            {
+                Loc = Location.Left;
+            }
+            if (x > 0 && x < Height && y > 0 && y < Width)
+            {
+                Loc = Location.Mid;
+            }
+            if (x > 0 && x < Height && y == Width)
+            {
+                Loc = Location.Right;
+            }
+
+            //Bottom Left, Bottom, and Bottom Right
+            if (x == Height && y == 0)
+            {
+                Loc = Location.BottomLeft;
+            }
+            if (x == Height && y > 0 && y < Width)
+            {
+                Loc = Location.Bottom;
+            }
+            if (x == Height && y == Width)
+            {
+                Loc = Location.BottomRight;
+            }
+        }
+    }
+}
